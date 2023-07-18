72078f8edc55b392d5623a92c83713b880221e1c
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-07-18 12:58:16 +0300

    Iterable Classes

---
+++ src/main/java/org/IterableClasslar/Main.java
@@ -0,0 +1,22 @@
+package org.IterableClasslar;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] kanallar = {"Radyo Viva", "Radyo Fenomen", "Kral Pop", "Alem Fm", "Radyo Hacettepe"};
+
+        Radyo radyo = new Radyo(kanallar);
+        /*
+        for (String s : radyo)
+            System.out.println(s);
+
+            */
+
+        Iterator<String> iterator = radyo.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+}
